9c9800460da4aa546fa84e20321c6608fcdbd94c
nudelx <alex.nudelman@samanage.com>
2017-07-02 16:50:23 +0300

    fixed

--- src/cards/textCard.js
+++ src/cards/textCard.js
@@ -62,14 +62,14 @@ class Card extends Component {
   }
 
   buildTimeStr(timestamp) {
-    const delta = new Date().getTime() / 1000 - timestamp
+    var delta = new Date().getTime() / 1000 - timestamp
     var days = Math.floor(delta / 86400);
     delta -= days * 86400;
     var hrs = Math.floor(delta / 3600) % 24;
     delta -= hrs * 3600;
     var mnts = Math.floor(delta / 60) % 60;
-    delta -= minutes * 60;
-    var secs = delta % 60;
+    delta -= mnts * 60;
+    var secs = Math.floor(delta % 60);
     return `${days ? `${days}d` : ''} ${hrs ? `${hrs}h` : ''} ${mnts
       ? `${mnts}m`
       : ''} ${secs ? `${secs}s` : ''} ago`
