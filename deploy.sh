010c410207d46a009b44db91e45be57dba1ed948
nudelx <alex.nudelman@samanage.com>
2017-07-05 11:48:27 +0300

    new deploy Wed Jul  5 11:48:27 IDT 2017

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+rm -rf static/
+yarn build
+cp -r build/* .
+sed  's/\="\//\="\.\//g' index.html > index1.html
+cat index1.html > index.html
+rm index1.html
+sed  's/n.p\+"static\/media\//"\.\/static\/media\//g' static/js/*.js > static/js/t.js
+file=$(ls static/js/*.js | grep main)
+cat  static/js/t.js >  $file
+rm static/js/t.js
+git add .
+git commit -am "new deploy $(date)"
+git push
