02a88793cd3bf62c036659db56148ad0ade5edb2
nudelx <alex.nudelman@samanage.com>
2017-07-09 17:05:09 +0300

    added prettier

--- src/cards/user.js
+++ src/cards/user.js
@@ -1,14 +1,22 @@
 import React from 'react'
 
-
 const User = ({ user }) => {
   const cleanUser = user.replace('/', '')
   return (
-    <div className='user-card'>
-      <div className='image-holder'><img alt="user" width="20px" height="20" src={`https://avatars1.githubusercontent.com/${cleanUser}`}/> </div>
-      <div className='name-holder'>{cleanUser}</div>
+    <div className="user-card">
+      <div className="image-holder">
+        <img
+          alt="user"
+          width="20px"
+          height="20"
+          src={`https://avatars1.githubusercontent.com/${cleanUser}`}
+        />{' '}
+      </div>
+      <div className="name-holder">
+        {cleanUser}
+      </div>
     </div>
   )
 }
 
-export default User;
+export default User
