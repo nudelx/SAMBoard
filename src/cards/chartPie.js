9a819d07f94352ae49f7c0e35ca7980af2b60e00
nudelx <alex.nudelman@samanage.com>
2017-08-21 14:39:42 +0300

    added pie

--- src/cards/chartPie.js
+++ src/cards/chartPie.js
@@ -6,20 +6,70 @@ const data = [
     color: "#F7464A",
     highlight:"#FF5A5E",
     label: "Red",
-    value: 122.03
+    value: 1.03
   },
   {
     color: "#F74ccc",
     highlight:"#FF5aaa",
     label: "Blue",
-    value: 33
+    value: 1
+  },
+  {
+    color: "#F7464A",
+    highlight:"#FF5A5E",
+    label: "Red",
+    value: 1
+  },
+  {
+    color: "#F74ccc",
+    highlight:"#FF5aaa",
+    label: "Blue",
+    value: 1
+  },
+  {
+    color: "#F7464A",
+    highlight:"#FF5A5E",
+    label: "Red",
+    value: 1
+  },
+  {
+    color: "#F74ccc",
+    highlight:"#FF5aaa",
+    label: "Blue",
+    value: 1
+  },
+  {
+    color: "#F7464A",
+    highlight:"#FF5A5E",
+    label: "Red",
+    value: 1
+  },
+  {
+    color: "#F74ccc",
+    highlight:"#FF5aaa",
+    label: "Blue",
+    value: 1
   }
 ]
+
+ const OPT = {
+  showTooltips: true,
+  segmentShowStroke: true,
+  percentageInnerCutout: 50,
+  segmentStrokeColor: '#e6e6e6',
+  animateRotate: true,
+  animationSteps: 75,
+  abelFontFamily : "Arial",
+  labelFontStyle : "normal",
+  labelFontSize : 24,
+  labelFontColor : "#666",
+  drawPieSegments: function () { return 33 ;}
+}
 
 const Pie = ({ }) => {
   return (
     <div>
-      <Doughnut width="600" height="250"  data={data}/>
+      <Doughnut width="100" height="100"  data={data} options={OPT} />
     </div>
   )
 }
