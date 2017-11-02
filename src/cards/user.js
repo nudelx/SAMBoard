b57c37d060050ee9cbe76c65600fcfe46025d968
Naftaly Rubin <naftaly.rubin@samanage.com>
2017-11-02 17:20:34 +0200

    css

--- src/cards/user.js
+++ src/cards/user.js
@@ -8,9 +8,9 @@ const User = ({ user, date }) => {
         <img
           alt="user"
           src={`https://avatars1.githubusercontent.com/${cleanUser}`}
-        />{' '}
+        />
       </div>
-      <div className="user-details">
+      <div className="user-holder">
         <div className="name-holder">
           {cleanUser}
         </div>
