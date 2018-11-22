456be041303b78a27f0c59c9f2e8c3831e9e53e1
nudelx <nudelx@gmail.com>
2018-11-22 15:32:05 +0200

    done

--- src/cards/BugCard.js
+++ src/cards/BugCard.js
@@ -17,49 +17,20 @@ export default class extends React.Component {
     const: {
       GOOD: 5,
       BAD: 10,
-      BUGS_URL: 'https://samanage.tpondemand.com/api/v1/Bugs',
-      BLOCKER_QUERY: "(Tags contains '*block*' )",
-      SSF_PREFIX: "(Project.Name contains 'SSF')",
-      SSP_PREFIX: "(Project.Name contains 'SSP')",
-      QUERY:
-        "(EntityState.Name not contains  'Code')and(EntityState.Id ne '174')and(EntityState.Id ne  '225')and(EntityState.Name not contains  '*Deploy*')and(EntityState.Name not contains  '*Design*')and(EntityState.Name not contains  '*Live*')",
-      LIMIT: 'take=100',
-      TOKEN:
-        'access_token=Mjk6YUdWZm9XbU8yemw1Q1BaMVBKZVEvL3VjdW1zeWN2YkJnVys3MHdocmthYz0='
+      BUGS_URL: 'https://nudel-proxy-node.herokuapp.com/',
+      SSF_PREFIX: 'SSF',
+      SSP_PREFIX: 'SSP'
     }
   }
 
   getData = () => {
     const { type } = this.props
-    const {
-      BUGS_URL,
-      BLOCKER_QUERY,
-      QUERY,
-      SSF_PREFIX,
-      SSP_PREFIX,
-      LIMIT,
-      TOKEN
-    } = this.state.const
+    const { BUGS_URL, SSF_PREFIX, SSP_PREFIX } = this.state.const
     axios
-      .get(
-        // "https://samanage.tpondemand.com/api/v1/Bugs?where=(Tags contains '*block*')&access_token=Mjk6YUdWZm9XbU8yemw1Q1BaMVBKZVEvL3VjdW1zeWN2YkJnVys3MHdocmthYz0="
-        `${BUGS_URL}?where=${BLOCKER_QUERY}and${QUERY}and${
-          type === 'SSP' ? SSP_PREFIX : SSF_PREFIX
-        }&${LIMIT}&${TOKEN}`,
-        {
-          method: 'GET',
-          mode: 'no-cors',
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          },
-          withCredentials: true,
-          credentials: 'same-origin'
-        }
+      .get(`${BUGS_URL}${type === 'SSP' ? SSP_PREFIX : SSF_PREFIX}`)
+      .then(resp =>
+        this.setState({ total: resp.data.Items.length, data: resp.data.Items })
       )
-      // .then(resp => resp.body)
-      .then(data => console.log(data))
-
-    //  this.setState({ total: resp.data.Items.length, data: resp.data.Items })
   }
   componentDidMount() {
     this.getData()
