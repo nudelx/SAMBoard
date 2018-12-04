0f4bf0ab135bcece667b7b87f0efadee33495cf7
Naftaly Rubin <naftaly.rubin@samanage.com>
2018-12-04 17:33:23 +0200

    Show dirty master user

--- src/header/statusBlock.js
+++ src/header/statusBlock.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { ENV_NAMES } from '../tools/envNames'
+import classnames from 'classnames'
 
 const StatusBlock = ({ data, envName }) => {
   data = data || {}
-  const { dirty } = data[envName] || 0
+  const { dirty, user } = data[envName] || 0
   const classStatus = dirty ? 'busy' : 'free'
-  const displaName = dirty ? 'dirty' : 'clean'
+  const displayStatus = dirty ? 'dirty' : 'clean'
+  const displayUser = user
   return (
     <div className={'statusBlock'}>
       <div className={'st-name'}>{ENV_NAMES[envName]}</div>
-      <div className={`st-value ${classStatus}`}>{displaName}</div>
+      <div className={`st-value ${classStatus}`}>{displayStatus}</div>
+      {dirty && <div className={classnames('st-user', 'busy')}>{displayUser}</div>}
     </div>
   )
 }
