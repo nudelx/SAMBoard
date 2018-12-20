4fb476bd3d09d4ac56308f7cd86ae98461ec14a4
Naftaly Rubin <naftaly.rubin@samanage.com>
2018-12-20 13:19:51 +0200

    handle tests severity colors

--- src/tests/testBox.js
+++ src/tests/testBox.js
@@ -1,7 +1,14 @@
 import React from 'react'
 
+const getSeverityStyle = (failed, total) => {
+  const percent = failed / total
+  if (percent < 5) return "test-minor"
+  if (percent >= 5 && percent < 10) return "test-major"
+  return "test-fail"
+}
+
 const TestBox = ({ name, failed, total }) => {
-  const statusClass = failed === 0 ? 'test-success' : 'test-fail'
+  const statusClass = failed === 0 ? "test-success" : getSeverityStyle(failed, total)
   return (
     <ul className={`test-thread-box ${statusClass}`}>
       <li className='test-thread-name'>{name}</li>
