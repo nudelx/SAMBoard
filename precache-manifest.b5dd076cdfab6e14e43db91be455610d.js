511f4abaee9a038ab5cb2709f661382dbb100a21
nudelx <nudelx@gmail.com>
2019-06-06 16:23:10 +0300

    new deploy Thu Jun  6 16:23:10 IDT 2019

---
+++ precache-manifest.b5dd076cdfab6e14e43db91be455610d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d052f277131a8eb926f0964b7729497",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b4c9821951efa7faca9",
+    "url": "/static/css/main.4714ad3c.chunk.css"
+  },
+  {
+    "revision": "610a736cce1f2424809d",
+    "url": "/static/js/2.f4a23db5.chunk.js"
+  },
+  {
+    "revision": "3b4c9821951efa7faca9",
+    "url": "/static/js/main.ca774170.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d65df7a68e65bb86851a6875553d5d4f",
+    "url": "/static/media/bb8.d65df7a6.gif"
+  }
+]);
