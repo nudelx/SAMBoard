3052db98e741dfa2014562541241c398f9ab8c17
nudelx <nudelx@gmail.com>
2019-06-10 10:08:28 +0300

    added polly

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import 'raf/polyfill'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import './weather.css'
