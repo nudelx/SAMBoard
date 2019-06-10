73cf8eebeae5eb27ff709d43bb96b5e83b18d470
nudelx <nudelx@gmail.com>
2019-06-10 10:10:39 +0300

    raf  [pollyy

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import 'raf/polyfill'
 import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import './weather.css'
+console.log('swsdw', t)
 
 ReactDOM.render(<App />, document.getElementById('root'))
 registerServiceWorker()
