92dc7802f331069ecae7b4564119b3c7aecdb18b
nudelx <nudelx@gmail.com>
2019-11-14 22:01:13 +0200

    mock fb

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+jest.mock('firebase')
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
