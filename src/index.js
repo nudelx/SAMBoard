895f8790b648900a77c372c454eb6c99aefdb5ff
nudelx <nudelx@gmail.com>
2019-11-18 16:17:06 +0200

    remove service

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import registerServiceWorker from './registerServiceWorker'
+// import registerServiceWorker from './registerServiceWorker'
 import './index.css'
 import './weather.css'
 import findPolyfill from './polyfill/find'
@@ -9,4 +9,4 @@ import findPolyfill from './polyfill/find'
 findPolyfill()
 
 ReactDOM.render(<App />, document.getElementById('root'))
-registerServiceWorker()
+// registerServiceWorker()
